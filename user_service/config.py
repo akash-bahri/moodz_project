fb08b7ac7d5adc574a1fae39e57383a474947e5c
AkashBahri <bahri.a@northeastern.edu>
2024-11-21 17:05:55 -0500

    debug

--- user_service/config.py
+++ user_service/config.py
@@ -1,8 +1,8 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+# AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+# SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+# ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
